aae1dbe2fce9865ebf73f501212f3a520857b770
wuyeyisheng <wuyeyisheng@qq.com>
2019-08-28 10:03:43 +0800

    8.28 上传昨天写的数据，主要把项目写完整，并将相关的路由完成

--- App/models.py
+++ App/models.py
@@ -1,26 +1,93 @@
 # 模型
+import datetime
 
 from .exts import db
 
 
+# commend = db.Table(
+#     'commend',
+#     db.Column(db.Integer, primary_key=True, autoincrement=True),
+#     db.Column('user_id', db.Integer, db.ForeignKey("User.id")),
+#     db.Column('article_id', db.Integer, db.ForeignKey("Article.id")),
+#     db.Column('info', db.TEXT, default=''),
+#     db.Column('time', db.DateTime, default=datetime.datetime.now())
+# )
+
+
 class User(db.Model):
-    # 表名
+    '''
+        用户表
+    '''
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(30), unique=True)
     password = db.Column(db.String(255))
     info = db.Column(db.TEXT)
+    icon = db.Column(db.String(255))  # 用户的图像。
     time = db.Column(db.DateTime)
     is_super = db.Column(db.Boolean)
     is_delete = db.Column(db.Boolean)
 
+    articles = db.relationship('Article', backref='ariticle_user', lazy=True)
+    comments = db.relationship('Commend', backref='comment_user', lazy=True)
 
-class Ariticle(db.Model):
-    # 文章表
-    __tablename__ = 'article'
+
+class AriticleType(db.Model):
+    '''
+        文章类型
+    '''
+    __tablename__ = 'articletype'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name =
+    name = db.Column(db.String(25))
+    othername = db.Column(db.String(25))  # 别名
+    time = db.Column(db.DateTime)
+    # 父类的ID
+    father_type = db.Column(db.Integer, default=False)
+
+    articles = db.relationship('Article', backref='ariticletype', lazy=True)
 
 
+class Article(db.Model):
+    '''
+        文章表
+    '''
+    __tablename__ = "article"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userid = db.Column(db.Integer, db.ForeignKey(User.id))
+    title = db.Column(db.String(50))
+    text = db.Column(db.TEXT)
+    time = db.Column(db.DateTime)
+    picture = db.Column(db.String(255))
+    num = db.Column(db.Integer) # 点击量
+    introduce = db.Column(db.String(255)) # 简介
+    type = db.Column(db.Integer, db.ForeignKey(AriticleType.id))
+
+    is_recommand = db.Column(db.Boolean)
+    # 评论的字段
+    commend = db.relationship('Commend', backref='article', lazy=True)
 
 
+class Photo(db.Model):
+    '''
+        用户发布的照片墙。
+    '''
+    __tablename__ = 'photo'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    info = db.Column(db.TEXT)
+    picture = db.Column(db.String(255))
+    time = db.Column(db.DateTime)
+    type = db.Column(db.Integer)
+
+
+class Commend(db.Model):
+    '''
+        用户评论表
+    '''
+    __tablename__ = 'commend'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userid = db.Column(db.Integer, db.ForeignKey(User.id))
+    articleid = db.Column(db.Integer, db.ForeignKey(Article.id))
+
+    info = db.Column(db.TEXT)
+    time = db.Column(db.DateTime)
--- App/views.py
+++ App/views.py
@@ -1,5 +1,5 @@
 # url+视图函数
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from .models import *
 
 blog = Blueprint('blog', __name__)
@@ -8,12 +8,88 @@ admin = Blueprint('admin', __name__)
 
 @blog.route('/')
 def home():
-    return 'HOME'
+    # 用户的个人信息
+    user = User.query.all()[0]
+    # 查询所有的文章
+    arts = Article.query.all()
+    # 查询图片
+    ablums = Photo.query.all()
+    # 查看自己分类
+    types = AriticleType.query.all()
+
+    data = {
+        "user":user,
+        "arts":arts,
+        "ablums":ablums,
+        "types":types,
+    }
+
+    return render_template('blog/index.html', **data)
+
+@blog.route("/info/<id>")
+def info(id):
+    '''
+    文章详细内容
+    :return:
+    '''
+    article = Article.query.get(id)
+    # 查询所有的文章
+    arts = Article.query.all()
+    # 查看自己分类
+    types = AriticleType.query.all()
+    data = {
+        "article":article,
+        "arts":arts,
+        "types": types,
+    }
+    return render_template("blog/info.html", **data)
+
+@blog.route("/share/")
+def share():
+    '''
+    图片壁纸的分类
+    :return:
+    '''
+    return render_template("blog/share.html")
+
+@blog.route("/about/")
+def about():
+    '''
+    关于我
+    :return:
+    '''
+    return render_template("blog/about.html")
+
+@blog.route("/gbook/")
+def gbook():
+    '''
+    关于我
+    :return:
+    '''
+    return render_template("blog/gbook.html")
 
 
-@blog.route('/index/')
-def index():
-    return render_template('blog/index.html')
+@blog.route("/type/<id>")
+def clicktype(id):
+    '''
+    点击分类的接口
+    :param id:
+    :return:
+    '''
+    data = {
+
+    }
+    return jsonify()
+
+
+
+@blog.route("/articleType/")
+def articleType():
+
+    return jsonify()
+
+
+# ===============管理员摄图===============================
 
 
 # 管理首页
